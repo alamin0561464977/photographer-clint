a11d113da46a36ea78441eaa3fa3a82453801c2c
alamin0561464977 <amin23435678901315@gmail.com>
2022-11-07 22:05:19 +0300

    Added a Sign Up Form

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const handelSignUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photo, email, password);
+        form.reset();
+    };
     return (
-        <div>
-            SignUp
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content px-40 flex-col lg:flex-row-reverse">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handelSignUp} className="card-body">
+                        <h1 className="text-5xl font-bold">Sign Up now!</h1>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input name='name' required type="text" placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">User Photo URL</span>
+                            </label>
+                            <input name='photo' required type="text" placeholder="user photo url" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' required type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' required type="password" placeholder="password" className="input input-bordered" />
+                            {/* <label className="label">
+                                <a href="#" className="label-text-alt font-bold text-red-500 link link-hover">Forgot password?</a>
+                            </label> */}
+                            <label className="label">
+                                <Link to="/login" className="label-text-alt font-bold text-green-700 link link-hover">Login</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button type='submit' className="btn font-bold text-white bg-pink-800">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="text-center lg:text-left">
+                    <img className=' w-3/5' src="https://pathwayport.com/saasland/images/login@4x.png" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
